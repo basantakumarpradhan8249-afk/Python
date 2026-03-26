45da623b044fd14bc8b3b9c70be227ec14fd2854
Basanta Kumar Pradhan <basantakumarpradhan8249@gmail.com>
2026-03-26 13:04:34 +0530

    Add basic data types examples in 02_datatypes.py

---
+++ Full Python/02_datatypes.py
@@ -0,0 +1,15 @@
+a = 1 # a is an integer 
+
+b = 5.22 # b is a floating-point number
+
+c = "basanta" # c is a string
+
+d = False # d is a boolean variable
+
+e = None # e is a NoneType variable
+
+print("a is : ", a)
+print("b is : ", b)
+print("c is : ", c)
+print("d is : ", d)
+print("e is : ", e)
