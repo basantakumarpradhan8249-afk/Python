3ec361137b5059106b781d14bf4c713539eb8010
Basanta Kumar Pradhan <basantakumarpradhan8249@gmail.com>
2026-03-28 12:00:31 +0530

    Create 04_operatores.py

---
+++ Full Python/04_operatores.py
@@ -0,0 +1,46 @@
+ #Arrethimatic operators
+a = 5
+b = 10 
+c = a+b
+print(c)
+
+# Assignment operators
+a = 5-2
+print(a)
+a += 3
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+
+#comparison operators``
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+    # logical peratores
+e = True or False
+
+# Truth table of 'or'
+
+print("true or false is", True or false)
+print("true or true is", True or false)
+print("false or false is", True or false)
+print("false or true is", True or false)
+
+# Truth table of 'AND'
+
+print("true and false is", True and False)
+print("true and true is", True and True)
+print("false and false is", False and False)
+print("false and true is", False and True)
+
+print (not(True))
