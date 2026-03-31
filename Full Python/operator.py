89cc2d2985f62e6c9f81b77956984b1d2512781c
Basanta Kumar Pradhan <basantakumarpradhan8249@gmail.com>
2026-03-31 20:52:52 +0530

    Create operator.py

---
+++ Full Python/operator.py
@@ -0,0 +1,10 @@
+# Write a python program to swap two numbers using user input.
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+
+a, b = b, a
+print("After swapping: ")
+print("Number 1: ", a)
+print("Number 2: ", b)
+
